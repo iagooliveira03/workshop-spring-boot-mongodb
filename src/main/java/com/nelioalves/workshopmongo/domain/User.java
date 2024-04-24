c04bc26ab5840049607d6e9761b131830ef8d367
Iago Oliveira <iagotcoliveira7@gmail.com>
2024-04-24 16:10:48 -0300

    Referenciando os posts do usuario

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,23 +1,28 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	 
+	
 	@Id
 	private String id;
 	private String name;
 	private String email;
 	
-	public User() { 
-		
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
+	public User() {
 	}
+
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
@@ -48,10 +53,23 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -61,11 +79,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-	
-	
-	
 }
-
